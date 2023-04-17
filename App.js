13f0ef1aa1e113a0f045a2a84c001165025ea401
tapiaht <hiver.tapia@uab.edu.bo>
2023-04-17 00:15:05 -0400

    Primercommit

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import 'react-native-gesture-handler';
+// import { StatusBar } from 'expo-status-bar';
+// import 'android.os.Bundle';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+// import NavigationStack from './src/navigation/NavigationStack';
+import NavigationTab from './src/navigation/NavigationTab';
+import NavigationDrawer from './src/navigation/NavigationDrawer';
+// import LoginForm from './src/components/LoginForm';
+// import Saludar from './src/components/Saludar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+{/*     <View style={styles.container}>
+      <Text>Hola mundo</Text>
+      <LoginForm/>
+      <Saludar/>
+      <Saludar firstname="Maria" lastname="Choque"/>
+    </View> */}
+    {/* <NavigationStack/> */}
+    <NavigationTab/>
+    <NavigationDrawer/>
+
+    </NavigationContainer>
   );
 }
 
