cee254ce03df20f2597fd6f0e942128d193b91f4
tapiaht <hiver.tapia@uab.edu.bo>
2023-04-17 23:51:57 -0400

    menu completo

--- App.js
+++ App.js
@@ -2,37 +2,38 @@
 // import { StatusBar } from 'expo-status-bar';
 // import 'android.os.Bundle';
 import 'react-native-gesture-handler';
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+//import React from 'react';
+//import { StyleSheet } from 'react-native';
+//import { NavigationContainer } from '@react-navigation/native';
 // import NavigationStack from './src/navigation/NavigationStack';
-import NavigationTab from './src/navigation/NavigationTab';
-import NavigationDrawer from './src/navigation/NavigationDrawer';
+//import NavigationTab from './src/navigation/NavigationTab';
+//import NavigationDrawer from './src/navigation/NavigationDrawer';
 // import LoginForm from './src/components/LoginForm';
 // import Saludar from './src/components/Saludar';
+import React, { createRef } from 'react'
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+
+import DrawerNavigator from './src/navigation/DrawerNavigator'
+
+const navigationRef = createRef()
+const nav = () => navigationRef.current
 
 export default function App() {
   return (
-    <NavigationContainer>
-{/*     <View style={styles.container}>
-      <Text>Hola mundo</Text>
-      <LoginForm/>
-      <Saludar/>
-      <Saludar firstname="Maria" lastname="Choque"/>
-    </View> */}
-    {/* <NavigationStack/> */}
-    <NavigationTab/>
-    {/* <NavigationDrawer/> */}
-
-    </NavigationContainer>
+<SafeAreaView style={styles.safeArea}>
+      <StatusBar barStyle="dark-content" />
+      <NavigationContainer ref={navigationRef}>
+          <DrawerNavigator nav={nav} />
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    overflow: 'hidden',
   },
-});
+})
+
