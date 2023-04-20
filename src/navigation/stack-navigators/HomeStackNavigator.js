7d72312761a8f9c2a3eb5655bc02f4c7542c73a7
tapiaht <hiver.tapia@uab.edu.bo>
2023-04-20 02:10:57 -0400

    con error

--- src/navigation/stack-navigators/HomeStackNavigator.js
+++ src/navigation/stack-navigators/HomeStackNavigator.js
@@ -3,12 +3,96 @@ import { View, Text } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import { screens } from '../RouteItems'
+import 'expo-dev-client'
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD7cEm_XyvGvcmVjMhf2nUyMg_QDLaaodY",
+  authDomain: "b360-e83bd.firebaseapp.com",
+  projectId: "b360-e83bd",
+  storageBucket: "b360-e83bd.appspot.com",
+  messagingSenderId: "819179015205",
+  appId: "1:819179015205:web:0bbc2f03beaeaa863603cc"
+};
+const app = initializeApp(firebaseConfig);
+GoogleSignin.configure({
+  scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+  webClientId: '<FROM DEVELOPER CONSOLE>', // client ID of type WEB for your server (needed to verify user ID and offline access)
+  offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  hostedDomain: '', // specifies a hosted domain restriction
+  forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+  accountName: '', // [Android] specifies an account name on the device that should be used
+  iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+  googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+  openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+  profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+});
+const db = getFirestore(app);
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
+signIn = async () => {
+  try {
+    await GoogleSignin.hasPlayServices();
+    const userInfo = await GoogleSignin.signIn();
+    this.setState({ userInfo });
+  } catch (error) {
+    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      // user cancelled the login flow
+    } else if (error.code === statusCodes.IN_PROGRESS) {
+      // operation (e.g. sign in) is in progress already
+    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      // play services not available or outdated
+    } else {
+      // some other error happened
+    }
+  }
+};
+const user = await GoogleSignin.addScopes({
+  scopes: ['https://www.googleapis.com/auth/user.gender.read'],
+});
+getCurrentUserInfo = async () => {
+  try {
+    const userInfo = await GoogleSignin.signInSilently();
+    this.setState({ userInfo });
+  } catch (error) {
+    if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+      // user has not signed in yet
+    } else {
+      // some other error
+    }
+  }
+};
+
+isSignedIn = async () => {
+  const isSignedIn = await GoogleSignin.isSignedIn();
+  this.setState({ isLoginScreenPresented: !isSignedIn });
+};
+
+getCurrentUser = async () => {
+  const currentUser = await GoogleSignin.getCurrentUser();
+  this.setState({ currentUser });
+};
 const Stack = createStackNavigator()
 
 const Home = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text>Home screen!</Text>
+    {/* <GoogleSigninButton
+  style={{ width: 192, height: 48 }}
+  size={GoogleSigninButton.Size.Wide}
+  color={GoogleSigninButton.Color.Dark}
+  onPress={this._signIn}
+  disabled={this.state.isSigninInProgress}
+/>; */}
   </View>
 )
 
